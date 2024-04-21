0c243f6e989d458e48935e5361fc0f16093aa436
YoSoyRev <emmanuel2303cp@gmail.com>
2024-04-21 11:33:58 -0600

    primer comit

---
+++ Fundamentos/assets/js/alerts.js
@@ -0,0 +1,8 @@
+// alert("Hola mundo");
+
+// let nombre = prompt('Cual es tu nombre');
+// console.log(nombre)
+// console.log( '****' + nombre + '****');
+
+const selection = confirm('Estas seguro de ser dev nodejs');
+console.log(selection);
---
+++ Fundamentos/assets/js/app.js
@@ -0,0 +1,27 @@
+
+// alert("Hola desde app.js")
+
+// console.log("hola mundo");
+
+let a = 10;
+let b = 120;
+let c = "Emmanuel ";
+let d = "Gambito";
+let x = a + b
+
+const saludo = c + d;
+
+// console.log( { x } );
+// console.log( { b } );
+// console.log( { c } );
+
+c = "Hola de nuevo"
+
+// console.table({a,b,c,d,x});
+
+let outerWidth = 10000;
+const outerHeight = 100;
+
+
+let miNuevoNombre = "Gambito Monami";
+console.log(miNuevoNombre)
